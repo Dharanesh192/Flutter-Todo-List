50daef93aa2c38c3a9337e464de52da17912216c
Dharanesh V <visudharaneshpranesh@gmail.com>
2026-03-14 09:49:14 +0530

    Container

--- Container.dart
+++ Container.dart
@@ -14,21 +14,29 @@ class Taskview extends StatefulWidget {
 class _TaskviewState extends State<Taskview>{
   @override
   Widget build(BuildContext context) {
-    return Container(
-                margin: EdgeInsets.only(top: 20),
-                width: MediaQuery.of(context).size.width * 0.8,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 22, 27, 34),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                height: 500,
-                child: Center(
-                  child: Text("No tasks yet",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),),
-                ),
-              );
+    return Column(
+            children: [
+              Container(
+  margin: EdgeInsets.only(top: 20),
+  width: MediaQuery.of(context).size.width * 0.8,
+  decoration: BoxDecoration(
+    color: const Color.fromARGB(255, 22, 27, 34),
+    borderRadius: BorderRadius.circular(10),
+  ),
+  height: MediaQuery.of(context).size.width > 600
+      ? MediaQuery.of(context).size.height * 0.7 
+      : MediaQuery.of(context).size.height * 0.6, 
+                    child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('Task ${index + 1}'),
+                          subtitle: Text('Task description'),
+                          leading: Icon(Icons.check_circle_outline),
+                        );
+                      },
+                    ),
+              ),
+      ],
+    );
 }}
